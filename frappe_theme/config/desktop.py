8dac57b5dc9137fe3fe5bf413df8b660d871b440
Rushabh Mehta <rmehta@gmail.com>
2014-05-03 16:16:27 +0530

    first commit

---
+++ frappe_theme/config/desktop.py
@@ -0,0 +1,9 @@
+from frappe import _
+
+data = {
+	"Frappe Theme": {
+		"color": "orange",
+		"icon": "icon-globe",
+		"label": _("Frappe Theme")
+	}
+}
---
+++ frappe_theme/hooks.py
@@ -0,0 +1,83 @@
+app_name = "frappe_theme"
+app_title = "Frappe Theme"
+app_publisher = "Web Notes"
+app_description = "Theme for Frappe sites"
+app_icon = "icon-globe"
+app_color = "orange"
+app_email = "info@frappe.io"
+app_url = "https://frappe.io"
+app_version = "0.0.1"
+
+# Includes in <head>
+# ------------------
+
+# include js, css files in header of desk.html
+# app_include_css = "/assets/frappe_theme/css/frappe_theme.css"
+# app_include_js = "/assets/frappe_theme/js/frappe_theme.js"
+
+# include js, css files in header of web template
+web_include_css = "/assets/frappe_theme/css/frappe_theme.css"
+# web_include_js = "/assets/frappe_theme/js/frappe_theme.js"
+
+# Installation
+# ------------
+
+# before_install = "frappe_theme.install.before_install"
+# after_install = "frappe_theme.install.after_install"
+
+# Desk Notifications
+# ------------------
+# See frappe.core.notifications.get_notification_config
+
+# notification_config = "frappe_theme.notifications.get_notification_config"
+
+# Permissions
+# -----------
+# Permissions evaluated in scripted ways
+
+# permission_query_conditions = {
+# 	"Event": "frappe.core.doctype.event.event.get_permission_query_conditions",
+# }
+#
+# has_permission = {
+# 	"Event": "frappe.core.doctype.event.event.has_permission",
+# }
+
+# Document Events
+# ---------------
+# Hook on document methods and events
+
+# doc_events = {
+# 	"*": {
+# 		"on_update": "method",
+# 		"on_cancel": "method",
+# 		"on_trash": "method"
+#	}
+# }
+
+# Scheduled Tasks
+# ---------------
+
+# scheduler_events = {
+# 	"all": [
+# 		"frappe_theme.tasks.all"
+# 	],
+# 	"daily": [
+# 		"frappe_theme.tasks.daily"
+# 	],
+# 	"hourly": [
+# 		"frappe_theme.tasks.hourly"
+# 	],
+# 	"weekly": [
+# 		"frappe_theme.tasks.weekly"
+# 	]
+# 	"monthly": [
+# 		"frappe_theme.tasks.monthly"
+# 	]
+# }
+
+# Testing
+# -------
+
+# before_tests = "frappe_theme.install.before_tests"
+
