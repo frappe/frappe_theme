920bc3a80ffb13fbcdce70f730c9320dfe64f57e
Faris Ansari <netchamp.faris@gmail.com>
2019-05-19 20:13:01 +0530

    fix: Smooth scroll anchor hash

--- frappe_theme/public/js/frappe_theme.js
+++ frappe_theme/public/js/frappe_theme.js
@@ -1,4 +1,4 @@
-frappe.get_modal = function(title, content) {
+frappe.get_modal = function (title, content) {
 	return $(
 		`<div class="modal" tabindex="-1" role="dialog">
 			<div class="modal-dialog" role="document">
@@ -21,3 +21,55 @@ frappe.get_modal = function(title, content) {
 		</div>`
 	);
 };
+
+frappe.ready(() => {
+	// Select all links with hashes
+	$('a[href*="#"]')
+		// Remove links that don't actually link to anything
+		.not('[href="#"]')
+		.click(function (event) {
+			// On-page links
+			if (
+				location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+				&& location.hostname == this.hostname
+			) {
+				// Figure out element to scroll to
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+				// Does a scroll target exist?
+				if (target.length) {
+					// Only prevent default if animation is actually gonna happen
+					event.preventDefault();
+					scroll_to_el(target, () => {
+						// Callback after animation
+						// Must change focus!
+						var $target = $(target);
+						$target.focus();
+						if ($target.is(":focus")) { // Checking if the target was focused
+							return false;
+						} else {
+							$target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+							$target.focus(); // Set focus again
+						};
+						// set window hash
+						window.history.pushState({}, '', this.hash);
+					});
+				}
+			}
+		});
+
+	$(window).on('load', () => {
+		let hash = window.location.hash;
+		if (hash) {
+			let el = document.querySelector(hash);
+			scroll_to_el(el);
+		}
+	});
+
+	function scroll_to_el(el, callback) {
+		let subnav_height = $('.subnav').outerHeight() || 0;
+		let offset_top = $(el).offset().top;
+		let position = offset_top - subnav_height;
+		$('html, body').animate({ scrollTop: position }, 500, callback);
+	}
+})
