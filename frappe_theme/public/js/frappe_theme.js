a7f45b530a29262c6da02be24e1396411fc8e930
Faris Ansari <netchampfaris@users.noreply.github.com>
2019-07-15 16:57:04 +0530

    fix: Use frappe as base for theme (#29)

--- frappe_theme/public/js/frappe_theme.js
+++ frappe_theme/public/js/frappe_theme.js
@@ -1,27 +1,3 @@
-frappe.get_modal = function (title, content) {
-	return $(
-		`<div class="modal" tabindex="-1" role="dialog">
-			<div class="modal-dialog" role="document">
-				<div class="modal-content">
-					<div class="modal-header">
-						<h5 class="modal-title">${title}</h5>
-							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-							<button type="button" class="btn btn-primary hide"></button>
-							<span aria-hidden="true">&times;</span>
-						</button>
-					</div>
-					<div class="modal-body py-4">
-						${content}
-					</div>
-					<div class="modal-footer hidden">
-						<button type="button" class="btn btn-primary hidden"></button>
-					</div>
-				</div>
-			</div>
-		</div>`
-	);
-};
-
 frappe.ready(() => {
 	// Select all links with hashes
 	$('a[href*="#"]')
@@ -81,4 +57,4 @@ frappe.ready(() => {
 		let position = offset_top - subnav_height;
 		$('html, body').animate({ scrollTop: position }, 500, callback);
 	}
-})
+});
